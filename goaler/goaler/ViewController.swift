3f7277c8150488195acdc73d893329660aa91632
Ricky Padilla <rickycpadilla@gmail.com>
2017-04-26 11:27:06 -0600

    started xcode project

---
+++ goaler/goaler/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  goaler
+//
+//  Created by Ricky Padilla on 4/26/17.
+//  Copyright © 2017 Ricky Padilla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
